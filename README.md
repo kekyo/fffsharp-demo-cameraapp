f497ab22199e6f8e913d810ce74ecf15020dcaf9
Kouji Matsui <k@kekyo.net>
2022-10-28 19:47:09 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,39 @@
+# 【第7回】.NET 7 x FUN FAN F# | Hybrid ライブデモコード
+
+## これは何？
+
+[【第7回】.NET 7 x FUN FAN F# | Hybrid](https://fun-fan-fsharp.connpass.com/event/256240/)で、ライブデモンストレーションとして、1からF#でカメラアプリケーションを書いたもののサンプル実装です。
+ここに上げてあるものは、あらかじめ作成したものですが、当日はこれと同じ内容のものを最初から構築します。
+
+* F#で書いています。
+* GUIフレームワークとして、WPFを使っています。
+* MVVMフレームワークとして、[Epoxy](https://github.com/kekyo/Epoxy)を使っています。
+* カメラキャプチャライブラリとして、[FlashCap](https://github.com/kekyo/FlashCap)を使っています。
+* プレビューの表示に、[SkiaImageView](https://github.com/kekyo/SkiaImageView)を使っています。
+
+実際のライブデモンストレーションは[YouTubeで公開](https://youtu.be/bhXDUpG8xSg)されます。
+
+## ライセンス
+
+MIT
+
+------
+
+# [Part 7] .NET 7 x FUN FAN F# | Hybrid Live Demo Code
+
+## What is this?
+
+This is a sample implementation of a camera application written in F# from scratch as a live demonstration in [[Part 7].NET 7 x FUN FAN F# | Hybrid (ja)](https://fun-fan-fsharp.connpass.com/event/256240/).
+The one shown here was created in advance, but the same content will be built from scratch on the day of the event.
+
+* This is written in F#.
+* WPF is used as a GUI framework.
+* We are using [Epoxy](https://github.com/kekyo/Epoxy) as MVVM framework.
+* [FlashCap](https://github.com/kekyo/FlashCap) is used as a camera capture library.
+* [SkiaImageView](https://github.com/kekyo/SkiaImageView) is used to display the preview.
+
+An actual live demonstration is available on [YouTube (ja)](https://youtu.be/bhXDUpG8xSg).
+
+## License
+
+MIT
---
+++ cameraapp/ViewModels/MainWindowViewModel.fs
@@ -0,0 +1,52 @@
+﻿////////////////////////////////////////////////////////////////////////////
+//
+// Epoxy template source code.
+// Write your own copyright and note.
+// (You can use https://github.com/rubicon-oss/LicenseHeaderManager)
+//
+////////////////////////////////////////////////////////////////////////////
+
+namespace cameraapp.ViewModels
+
+open Epoxy
+open FlashCap
+open SkiaSharp
+
+[<Sealed; ViewModel>]
+type public MainWindowViewModel() as self =
+    do
+        // A handler for window loaded
+        self.Ready <- Command.Factory.create(fun () -> async {
+            let devices = new CaptureDevices()
+            let deviceDescriptors = devices.enumerateDescriptors() |> Seq.toArray
+            let deviceDescriptor = deviceDescriptors.[0]
+            let characteristics = deviceDescriptor.Characteristics.[0]
+
+            let! device = deviceDescriptor.openAsync(
+                characteristics,
+                fun pixelBufferScope -> async {
+                    let imageData = pixelBufferScope.Buffer.referImage()
+                    let bitmap = SKBitmap.Decode(imageData)
+                    do pixelBufferScope.releaseNow()
+                    do! UIThread.bind()
+                    self.PreviewImage <- bitmap
+                })
+            self.device <- device
+
+            device.start()
+
+            self.IsEnabled <- true
+        })
+
+        // A handler for fetch button
+        self.Fetch <- CommandFactory.create(fun () -> async {
+            do self.IsEnabled <- false
+        })
+
+    [<DefaultValue>]
+    val mutable device: CaptureDevice
+
+    member val Ready: Command = null with get, set
+    member val IsEnabled: bool = false with get, set
+    member val Fetch: Command = null with get, set
+    member val PreviewImage: SKBitmap = null with get, set
